1ba3ca86c9a055749b8e7075792c9fd77239bfff
yimmit303 <timsteiner303@gmail.com>
2019-09-28 14:40:30 -0400

    Made it so you can transition levels with F

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 var script
-var level
 
 var vel = Vector2(0,0)
 var max_speed = 200
@@ -24,6 +23,8 @@ func _process(delta):
 
 func _input(event):
 	script.input(self, event)
+	if event is InputEventKey and event.scancode == KEY_F and event.is_pressed() and !event.is_echo():
+		transition(self)
 	pass
 
 
