14b6fd334d82804a97770afa5638b428c3467ce0
benm-way <55898632+benm-way@users.noreply.github.com>
2019-09-27 20:58:08 -0400

    glitches 1 and 2

---
+++ Scripts/player_level1.gd
@@ -0,0 +1,12 @@
+extends Node
+
+static func move(mover, delta):
+	# Anything and everything related to movement goes in here
+	if Input.is_key_pressed(KEY_F):
+		mover.position += Vector2(100 * delta, 0)
+	if Input.is_key_pressed(KEY_S):
+		mover.position += Vector2(-100 * delta, 0)
+
+static func jump(mover, event):
+	# Anything that happens when the player jumps happens here
+	pass
