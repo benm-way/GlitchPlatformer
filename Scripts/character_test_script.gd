95b04ea75131c6150f9b3a51295f02728b4341ee
Robert Howard <55898698+NamelessPeon@users.noreply.github.com>
2019-09-27 21:40:47 -0400

    ah, push it

--- Scripts/character_test_script.gd
+++ Scripts/character_test_script.gd
@@ -1,21 +1,49 @@
-extends KinematicBody2D
+extends Node
 
-export (int) var speed = 200
+static func move(mover, delta):
+	# Anything and everything related to movement goes in here
+	var dir = Vector2(0,0)
+	
+	if Input.is_key_pressed(KEY_D):
+		dir.x += 1
+	if Input.is_key_pressed(KEY_A):
+		dir.x -= 1
+	if Input.is_key_pressed(KEY_SPACE) and mover.is_on_floor():
+		mover.vel.y = -2000
+	
+	
+	dir = dir.normalized()
+	
+	var hvel = mover.vel
+	hvel.y = 0
+	
+	var target = dir * mover.max_speed
 
-var velocity = Vector2()
+	
+	var accel
+	
+	if (dir.dot(hvel) > 0):
+		accel = mover.ACCEL
+	else:
+		accel = mover.DEACCEL
+		
+	hvel = hvel.linear_interpolate(target, accel*delta)
+	mover.vel.x = hvel.x
+	
+	# Jumping
+	if (mover.vel.y < 0):
+		mover.vel.y += mover.GRAVITY * mover.JUMP_MULT
+	elif (mover.vel.y >= 0 and !Input.is_key_pressed(KEY_SPACE)):
+		mover.vel.y += mover.GRAVITY * mover.LOW_JUMP_MULT
+	mover.vel.y += mover.GRAVITY
+	
+	mover.vel = mover.move_and_slide(mover.vel, Vector2(0,-1))
 
-func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('right'):
-        velocity.x += 1
-    if Input.is_action_pressed('left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
+# For something called every frame
+static func process(player, delta):
+	pass
 
-func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+# For something that happens on an input event
+static func input(player, event):
+	pass
+	
